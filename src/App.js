16b7225be583066be384a0c3946f229eccc84f35
Max <max.zastavny@gmail.com>
2022-01-18 14:38:32 +0100

    clean up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Hi, I am the React app.</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1> Hi, I am the React app.</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
