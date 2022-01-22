796ba94f10d767cdb2cdaa8cb88451282753fb54
Max <max.zastavny@gmail.com>
2022-01-22 17:06:41 +0100

    update packages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { Component } from 'react';
 import './App.css';
+import Signup from './Auth/Signup';
+import Login from './Auth/Login';
 import Measure from './Measure/Measure';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Signup></Signup>
+        <Login></Login> 
         <Measure name="electricity" />
       </div>
     );
