fec81a6a3da19d0f371c0eee79991bf5e8167996
Max <max.zastavny@gmail.com>
2022-01-23 15:40:13 +0100

    output data

--- src/Measure/Measure.js
+++ src/Measure/Measure.js
@@ -1,8 +1,10 @@
-import axios from "../axios-api";
+import React, {useState} from "react";
 import Amplify, { API } from 'aws-amplify';
 import { Auth } from "aws-amplify";
 
-const measure = (props) => {
+const Measure = (props) => {
+    const [email, setEmail] = useState("")
+    const [measure, setMeasure] = useState("")   
 
     const getData = async () => { 
         const apiName = 'ServiceApi';
@@ -10,62 +12,28 @@ const measure = (props) => {
         const myInit = { 
           headers: { 
             Authorization: `Bearer ${(await Auth.currentSession()).getIdToken().getJwtToken()}`,
-            // "Access-Control-Allow-Credentials" : true,
-            // "Access-Control-Allow-Origin": "*",
-            // "Access-Control-Allow-Headers": "Content-Type",
-            // "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
           },
         };
     
         return await API.get(apiName, path, myInit)
             .then(response => {
                 console.log(response);
+                setEmail(response.email)
+                setMeasure(response.output.data)
             })
             .catch(error => {
                 console.log(error);
             });;
     }
 
-    // const getHandler = () => {
-    //     // axios.get()
-    //     //     .then(response => console.log(response))
-    //     //     .catch(error => console.log(error))
-    //     // const myInit = { // OPTIONAL
-    //     //     // headers: {}, // OPTIONAL
-    //     //     // headers: {
-    //     //     //     "Access-Control-Allow-Headers" : "Content-Type",
-    //     //     //     "Access-Control-Allow-Origin": "https://www.example.com",
-    //     //     //     "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
-    //     //     // },
-    //     //     headers: {
-    //     //         "Access-Control-Allow-Credentials": "True",
-    //     //         "Access-Control-Allow-Headers": "Content-Type",
-    //     //         "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
-    //     //         "Access-Control-Allow-Origin": "*",
-    //     //     }
-    //     // };
-    //     const myInit = { 
-    //         headers: { 
-    //           Authorization: `Bearer ${(await Auth.currentSession()).getIdToken().getJwtToken()}`,
-    //         },
-    //       };
-
-    //     API.get("ServiceApi", "/Stage", "")
-    //         .then(response => {
-    //             console.log(response);
-    //         })
-    //         .catch(error => {
-    //             console.log(error.response);
-    //         });
-    // }
-
     const value = Math.random() * 10
     return (
         <div>
-            <p> I am a {props.name} measure: {value} </p>
+            <p> I am a {props.name} measure: {measure} </p>
+            <p> Requested by user: {email} </p>
             <button onClick={async () => getData()}> Click to get data </button>
         </div>
     );
 }
 
-export default measure; 
+export default Measure; 
