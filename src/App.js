89a4c460c9e99689c09c5a70abd78d335d4abfe8
Max <max.zastavny@gmail.com>
2022-01-30 19:08:38 +0100

    refactor project struct

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import styles from "./App.module.css"
-import Measure from './Measure/Measure';
-import Devices from "./Devices/Devices";
+import Layout from './components/Layout/Layout'
 
-import { Amplify } from 'aws-amplify';
-
-import { withAuthenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
-
-import awsExports from './aws-exports';
-Amplify.configure(awsExports);
-
-function App({ signOut, user }) {
+const App = (props) => {
   return (
-    <div className={styles.App}>
-      {/* <h1>Hello {user.username}</h1> */}
-      <Devices></Devices>
-      <button className={styles.Button} onClick={signOut}>Sign out</button>
-    </div>
+      <Layout>
+        <p>test</p>
+      </Layout>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
